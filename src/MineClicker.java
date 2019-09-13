04992f20a33571b7346c6fcb9fb29258b8e811fc
berth1832777 <berth1832777@etu.cegepjonquiere.ca>
2019-09-13 09:10:00 -0400

    Premier commit

---
+++ src/MineClicker.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MineClicker extends Application {
+
+    public static void main(String[] args) { launch(args);}
+
+    @Override
+    public void start (Stage primaryStage) {
+
+        primaryStage.setTitle("Minecraft Clicker");
+        primaryStage.setResizable(false);
+        primaryStage.show();
+
+    }
+
+}
